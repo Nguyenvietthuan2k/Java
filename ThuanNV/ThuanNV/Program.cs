209fa79044af7aa9f4c9620000abb977ecb4ac16
Nguyễn Viết Thuần <50479875+Nguyenvietthuan2k@users.noreply.github.com>
2019-05-30 11:25:58 +0700

    Add files via upload

---
+++ ThuanNV/ThuanNV/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThuanNV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yrsOfService = 3;
+            int salary = 1500;
+            int bonus = 0;
+
+            if (yrsOfService<5)
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ mag ket hop.php
@@ -0,0 +1,13 @@
+﻿<?php
+    $players = array(
+        "Quang hai" => "Ha noi Fc",
+        "Messi" => "Bacelona",
+        "Ronaldo" => "Juve",
+);
+foreach($players as $cauthu => $clb){
+    echo "Cầu thủ {$cauthu} da cho clb {$clb}";
+    echo "\n";
+};
+while(true);
+
+?>
