86256dba88decd0ea1ba585aa715ad9c61783102
Nguyễn Viết Thuần <50479875+Nguyenvietthuan2k@users.noreply.github.com>
2019-05-30 11:56:30 +0700

    Add files via upload

--- BT601/BT601/Program.cs
+++ BT601/BT601/Program.cs
@@ -11,31 +11,49 @@ namespace BT601
         static void Main(string[] args) {
             int choose;
             string name;
+            int toan;
+            int li;
+            int hoa;
             do
             {
-                Console.WriteLine("Nhap ten");
-                Console.WriteLine("Hien thi ten");
-                Console.WriteLine("Thoat");
-                Console.WriteLine("Nhap lua chon:");
+                Console.WriteLine("1.Nhap ho va ten");
+                Console.WriteLine("2.Nhap diem toan li hoa");
+                Console.WriteLine("3.Tinh tong diem 3 mon");
+                Console.WriteLine("4.Tinh trung binh mon");
+                Console.WriteLine("Chon:");
 
                 choose = int.Parse(Console.ReadLine());
 
                 switch (choose)
                 {
                     case 1:
-                        Console.WriteLine("Nhap ten:");
-                        name = Console.WriteLine();
+                        Console.WriteLine("Nhap ho ten:");
+                        name = Console.ReadLine();
                         break;
                     case 2:
-                        if(name!=null)
-                        Console.WriteLine("");
+                        Console.WriteLine("Nhap diem toan :");
+                        toan = f.Parse(Console.ReadLine());
+                        Console.WriteLine("Nhap diem li :");
+                        li = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Nhap diem hoa:");
+                        hoa = int.Parse(Console.ReadLine());
                         break;
                     case 3:
-                        Console.WriteLine("Thoat");
+                        Console.WriteLine("Nhap diem toan :");
+                        toan = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Nhap diem li :");
+                        li = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Nhap diem hoa:");
+                        hoa = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Tinh tong diem 3 mon{0}", toan + li + hoa);
+                        break;
+                    case 4:
+
+                        Console.WriteLine("Tinh trung binh{0}",(toan+li+hoa)/3);
                         break;
 
                 }
-            } while (choose != 4);
+            } while (choose != 5);
             Console.ReadLine();
         }                          
     }
